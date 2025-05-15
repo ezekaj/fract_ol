6f81613d063e20ee9f3568bd3d7185155e6b56a2
gasolineeater <elvizekaj02@gmail.com>
2025-05-15 09:40:25 +0200

    Test: Init of the blank window.

--- src/main.c
+++ src/main.c
@@ -1,13 +1,15 @@
 #include "../inc/fractol.h"
 
-#define WIDTH 800
-#define HEIGHT 500
+// #define WIDTH 800
+// #define HEIGHT 500
 
-int	main(int ac, char **av)
+int	main(void)
 {
 	mlx_t		*mlx;
 	mlx_image_t	*img;
 
 	mlx = mlx_init(WIDTH, HEIGHT, "Fractol", 1);
 	img = mlx_new_image(mlx, WIDTH, HEIGHT);
+	mlx_image_to_window(mlx, img, 0, 0);
+	mlx_loop(mlx);
 }
