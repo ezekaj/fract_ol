aff2405bccf4363607cb460e256ef177d148a384
gasolineeater <elvizekaj02@gmail.com>
2025-05-15 09:24:18 +0200

    Add: Init of mlx && mlx42 directory.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,13 @@
 #include "../inc/fractol.h"
 
+#define WIDTH 800
+#define HEIGHT 500
+
 int	main(int ac, char **av)
 {
+	mlx_t		*mlx;
+	mlx_image_t	*img;
 
+	mlx = mlx_init(WIDTH, HEIGHT, "Fractol", 1);
+	img = mlx_new_image(mlx, WIDTH, HEIGHT);
 }
