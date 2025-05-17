bad41a236bb873ba485999b2aeea6138133b43bb
gasolineeater <elvizekaj02@gmail.com>
2025-05-17 14:59:00 +0200

    Enhance: Improve command-line argument parsing and usage information

--- src/main.c
+++ src/main.c
@@ -2,8 +2,27 @@
 
 static void	print_usage(void)
 {
-	if (write(1, "Usage: ./fractol [fractal_type]\n", 33) < 0)
-		exit(1);
+	write(1, "Usage: ./fractol [fractal_type] [params]\n\n", 42);
+	write(1, "Fractal types:\n", 15);
+	write(1, "  mandelbrot\n", 13);
+	write(1, "  julia [c_real] [c_imag]\n\n", 27);
+	write(1, "Examples:\n", 10);
+	write(1, "  ./fractol mandelbrot\n", 23);
+	write(1, "  ./fractol julia -0.7 0.27015\n", 31);
+	exit(0);
+}
+
+static int	parse_args(int ac, char **av)
+{
+	if (ac < 2)
+		return (0);
+
+	if (ft_strncmp(av[1], "mandelbrot", 10) == 0)
+		return (MANDELBROT);
+	else if (ft_strncmp(av[1], "julia", 5) == 0)
+		return (JULIA);
+
+	return (0);
 }
 
 int	main(int ac, char **av)
@@ -11,28 +30,33 @@ int	main(int ac, char **av)
 	mlx_t		*mlx;
 	mlx_image_t	*img;
 	t_fractol	fractol;
+	int			type;
 
-	(void)av; // Suppress unused parameter warning
-
-	if (ac >= 2)
-	{
-		mlx = mlx_init(WIDTH, HEIGHT, "Fractol", 1);
-		if (!mlx)
-			return (1);
-		img = mlx_new_image(mlx, WIDTH, HEIGHT);
-		if (!img)
-		{
-			mlx_terminate(mlx);
-			return (1);
-		}
-		fractol.mlx = mlx;
-		fractol.img = img;
-		mlx_image_to_window(mlx, img, 0, 0);
-		fractal_render(&fractol);
-		mlx_loop(mlx);
-		mlx_terminate(mlx);
-	}
-	else
+	type = parse_args(ac, av);
+	if (!type)
 		print_usage();
+
+	mlx = mlx_init(WIDTH, HEIGHT, TITLE, 1);
+	if (!mlx)
+		return (1);
+	img = mlx_new_image(mlx, WIDTH, HEIGHT);
+	if (!img)
+	{
+		mlx_terminate(mlx);
+		return (1);
+	}
+
+	fractol.mlx = mlx;
+	fractol.img = img;
+	init_fractol(&fractol, type, av);
+
+	mlx_image_to_window(mlx, img, 0, 0);
+	mlx_key_hook(mlx, &handle_keys, &fractol);
+	mlx_scroll_hook(mlx, &handle_scroll, &fractol);
+
+	fractal_render(&fractol);
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+
 	return (0);
 }
