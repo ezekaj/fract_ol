d04b9827dd17ab0e95184326d314765ab1889ba5
gasolineeater <elvizekaj02@gmail.com>
2025-05-17 12:59:36 +0200

    Add: Directory for the fractals equations && init of mandelbrot.

---
+++ src/fractals/mandelbrot.c
@@ -0,0 +1,14 @@
+#include "../../inc/fractol.h"
+
+int	mandelbrot(double real, double imag, int max_i)
+{
+	double	z_real;
+	double	z_imag;
+	double	temp;
+	int		i;
+
+	z_imag = 0;
+	z_real = 0;
+	i = 0;
+	return (i);
+}
