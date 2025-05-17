8635bfb9c4c38553aad4cd886d154d7f20c517a2
gasolineeater <elvizekaj02@gmail.com>
2025-05-17 14:54:33 +0200

    Add: New fractal types and function prototypes to header

--- inc/fractol.h
+++ inc/fractol.h
@@ -12,15 +12,28 @@
 # define HEIGHT 600
 # define TITLE "Fractol"
 
+# define MANDELBROT 1
+# define JULIA 2
+
 typedef struct s_fractol
 {
 	mlx_t		*mlx;
 	mlx_image_t	*img;
+	int			type;
+	double		c_real;
+	double		c_imag;
+	double		zoom;
+	double		offset_x;
+	double		offset_y;
 }	t_fractol;
 
 void	map(int x, int y, double *real, double *imag, t_fractol *fractol);
 void	fractal_render(t_fractol *fractol);
-int	mandelbrot(double real, double imag, int max_i);
-int	colors(int i, int max_i);
+int		mandelbrot(double real, double imag, int max_i);
+int		julia(double real, double imag, double c_real, double c_imag, int max_i);
+int		colors(int i, int max_i);
+void	init_fractol(t_fractol *fractol, int type, char **av);
+void	handle_keys(mlx_key_data_t keydata, void *param);
+void	handle_scroll(double xdelta, double ydelta, void *param);
 
 # endif
