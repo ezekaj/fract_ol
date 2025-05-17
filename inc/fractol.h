fcd1b249f9597ae58e777593fdd2de2710022c53
gasolineeater <elvizekaj02@gmail.com>
2025-05-17 13:12:26 +0200

    Add: Function to set the coordinates of the map.

--- inc/fractol.h
+++ inc/fractol.h
@@ -18,6 +18,8 @@ typedef struct s_fractol
     mlx_image_t *img;
 } t_fractol;
 
+void	map(int x, int y, double *real, double *imag, t_fractol *fractol)
+
 int	mandelbrot(double real, double imag, int max_i)
 
 # endif
--- src/render.c
+++ src/render.c
@@ -1,3 +1,7 @@
 #include "../inc/fractol.h"
 
-
+void	map(int x, int y, double *real, double *imag, t_fractol *fractol)
+{
+	*real = (x - WIDTH / 2.0) * 3.0 / WIDTH - 0.5;
+	*imag = (y - HEIGHT / 2.0) * 3.0 / HEIGHT;
+}
