8ff18139fd46997859398f62d7af256e464664ab
gasolineeater <elvizekaj02@gmail.com>
2025-05-17 13:04:37 +0200

    Del: ft_strlen since we have it from libft.

--- src/utils.c
+++ src/utils.c
@@ -1,16 +1,2 @@
 #include "../inc/fractol.h"
 
-size_t	ft_strlen(char *s)
-{
-	int i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-
-// int	main(void)
-// {
-// 	printf("%zu\n", ft_strlen("123"));
-// }
