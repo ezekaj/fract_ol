9a44842b3c184b7dc11ffaa005621399afa3e1a8
gasolineeater <elvizekaj02@gmail.com>
2025-05-17 13:04:04 +0200

    Add: Implementation of the equation for mandelbrot.

--- src/fractals/mandelbrot.c
+++ src/fractals/mandelbrot.c
@@ -10,5 +10,12 @@ int	mandelbrot(double real, double imag, int max_i)
 	z_imag = 0;
 	z_real = 0;
 	i = 0;
+	while (z_real*z_real + z_imag*z_imag < 4 && i < max_i)
+	{
+		temp = z_real*z_real - z_imag*z_imag + real;
+		z_imag = 2*z_real*z_imag + imag;
+		z_real = temp;
+		i++;
+	}
 	return (i);
 }
