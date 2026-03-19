02943f7e5d82f5e88a235cb1eaa36503b782ea23
ezekaj <elvizekaj02@gmail.com>
2026-03-19 16:31:23 +0100

    feat: add bonus src/fractal_bonus.c with tricorn
    All 3 fractals store z_real/z_imag in struct before returning.
    Eliminates smooth_calc.c redundant recalculation.
    Tricorn: z_imag = -2*z_real*z_imag (conjugate).

---
+++ src/fractal_bonus.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractal_bonus.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ezekaj <ezekaj@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/17 15:30:38 by ezekaj            #+#    #+#             */
+/*   Updated: 2025/05/19 01:30:00 by ezekaj           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/fractol_bonus.h"
+
+int	mandelbrot(double real, double imag, t_fractol *f)
+{
+	double	z_real;
+	double	z_imag;
+	double	temp;
+	int		i;
+
+	z_real = 0;
+	z_imag = 0;
+	i = 0;
+	while (z_real * z_real + z_imag * z_imag < 4 && i < f->max_iter)
+	{
+		temp = z_real * z_real - z_imag * z_imag + real;
+		z_imag = 2 * z_real * z_imag + imag;
+		z_real = temp;
+		i++;
+	}
+	f->z_real = z_real;
+	f->z_imag = z_imag;
+	return (i);
+}
+
+int	julia(double real, double imag, t_fractol *f)
+{
+	double	z_real;
+	double	z_imag;
+	double	temp;
+	int		i;
+
+	z_real = real;
+	z_imag = imag;
+	i = 0;
+	while (z_real * z_real + z_imag * z_imag < 4 && i < f->max_iter)
+	{
+		temp = z_real * z_real - z_imag * z_imag + f->c_real;
+		z_imag = 2 * z_real * z_imag + f->c_imag;
+		z_real = temp;
+		i++;
+	}
+	f->z_real = z_real;
+	f->z_imag = z_imag;
+	return (i);
+}
+
+int	tricorn(double real, double imag, t_fractol *f)
+{
+	double	z_real;
+	double	z_imag;
+	double	temp;
+	int		i;
+
+	z_real = real;
+	z_imag = imag;
+	i = 0;
+	while (z_real * z_real + z_imag * z_imag < 4 && i < f->max_iter)
+	{
+		temp = z_real * z_real - z_imag * z_imag + real;
+		z_imag = -2 * z_real * z_imag + imag;
+		z_real = temp;
+		i++;
+	}
+	f->z_real = z_real;
+	f->z_imag = z_imag;
+	return (i);
+}
+
+int	calc_pixel(double real, double imag, t_fractol *f)
+{
+	if (f->type == MANDELBROT)
+		return (mandelbrot(real, imag, f));
+	if (f->type == TRICORN)
+		return (tricorn(real, imag, f));
+	return (julia(real, imag, f));
+}
